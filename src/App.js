41c91b98ddadf504486529717f5f7da271286d5b
Patrick Klitzke <philologos14@gmail.com>
2016-06-01 15:34:04 +0900

    sample app in OnsenUI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
+import 'onsenui'
+import {Page, Navigator, Button, Input} from 'react-onsenui'
+
+class PageTwo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      <div>
-      React test example.
-      </div>
+      <Page>
+        <div style={{padding: 20}}>
+          This is a second Page
+          <Button onClick={() => this.props.navigator.popPage()}> Pop </Button>
+        </div>
+      </Page>
+
     );
   }
 }
+
+class PageOne extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  onChangeText(event) {
+    this.setState({text: event.target.value});
+  }
+
+  pushPage() {
+    this.props.navigator.pushPage({
+      component: PageTwo
+    });
+  }
+
+  render() {
+    return (
+      <Page>
+        <div style={{padding: 20}}>
+          <Input
+            placeholder='Input field'
+            style={{height: 20}}
+            value={this.state.text}
+            onChange={this.onChangeText.bind(this)} /> <br />
+          <p>
+            You typed {this.state.text}
+          </p>
+          <Button onClick={this.pushPage.bind(this)}> Push page </Button>
+        </div>
+      </Page>
+
+    );
+  }
+};
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderPage(route, navigator) {
+    const props = route.props || {};
+    props.navigator = navigator;
+
+    return React.createElement(route.component, props);
+  }
+
+  render() {
+    return (
+    <Navigator
+      initialRoute={{
+        component: PageOne
+      }}
+      renderPage={this.renderPage.bind(this)}
+    />
+    );
+  }
+}
+
 
